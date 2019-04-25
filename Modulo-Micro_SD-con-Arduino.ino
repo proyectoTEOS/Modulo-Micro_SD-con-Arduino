01811f410409d82ec8305a0986adaff114c28b1b
Erik Velasco <proyectoteos@gmail.com>
2019-04-25 14:07:39 -0500

    Code and diagram available

--- Modulo-Micro_SD-con-Arduino.ino
+++ Modulo-Micro_SD-con-Arduino.ino
@@ -1,16 +1,3 @@
-# Modulo-Micro_SD-con-Arduino
-Guardaremos datos en un archivo desde una memoria micro-sd con Arduino.
-
-### Requisitos
-- Tener el IDE de [Arduino](https://www.arduino.cc/en/Main/Software) (o el de tu preferencia) con la versión más actual
-- Contar con las librería [SD](https://github.com/arduino-libraries/SD)
-- Tener el material para hacer el proyecto
-
-### Código
-**[Aqui](https://github.com/proyectoTEOS/Modulo-Micro_SD-con-Arduino/blob/master/Modulo-Micro_SD-con-Arduino.ino)** podrás obtener el link del código, también dejaremos
-una vista previa aquí abajo.
-
-```c++
 /*
   Created by TEOS
   Domotic with Arduino https://goo.gl/btZpjs
@@ -66,8 +53,3 @@ String filePrintT() {
   serialPrintT = String(sensor1T) + "," + String(sensor2T) + "," + String(sensor3T);
   return  serialPrintT;
 }
-```
-
-### Diagrama
-El siguiente esquemático muestra como se debe conectar todos los componentes con la placa.
-![](https://github.com/proyectoTEOS/Modulo-Micro_SD-con-Arduino/blob/master/modulo-micro_sd-con-arduino-5.jpg)
